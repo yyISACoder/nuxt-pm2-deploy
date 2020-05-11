df4f4521601fe72adedb38c5e4bc1b824754653f
yinyue <664960698@qq.com>
2020-05-11 15:38:44 +0800

    初始化

---
+++ ecosystem.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  apps : [{
+    name: 'API',
+    script: './server/index.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'root',
+      host : '106.53.46.97',
+      ref  : 'origin/master',
+      repo : 'git@github.com:yyISACoder/nuxt-pm2-deploy.git',
+      path : '/var/www/pm2-deploy',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
